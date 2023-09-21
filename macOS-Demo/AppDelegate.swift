4f40539d5866b0689646520d0c0f58ac445272d4
EhsanTgv <EhsanTgv@gmail.com>
2023-09-21 14:20:59 +0300

    Initial Commit

---
+++ macOS-Demo/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  macOS-Demo
+//
+//  Created by Ehsan Taghavi on 9/21/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
